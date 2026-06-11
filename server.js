e7967e9ee54370ef5486e29ec4e3c63889f3317c
softwereAyoub <ay.bouguern@gmail.com>
2026-06-11 21:39:21 +0100

    commit domain

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 
 // تفعيل الـ CORS لتسمح للـ Front-end (المرفوع على Vercel مثلاً) بالاتصال بالباك-إند
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://direop.com', 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
